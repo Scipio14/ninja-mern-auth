e45046f01195219b1a8af0d61d3fbd71f6237842
Federico Macías <fma.abogado@gmail.com>
2022-07-21 10:44:25 -0500

    Created the signup static method on the user model, the salt and password hash

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const {Schema,model} = require('mongoose');
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema({
   email:{
@@ -12,5 +13,21 @@ const userSchema = new Schema({
   }
 
 })
+
+//Static methods
+userSchema.statics.signup = async function (email,password){
+  const exists = await this.findOne({email});
+
+  if(exists){
+    throw Error("Email already exists")
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password,salt);
+
+  const user = await this.create({email,password:hash});
+
+  return user;
+
+}
 
 module.exports = model('User',userSchema);
