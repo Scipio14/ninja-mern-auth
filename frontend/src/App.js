04b2098178ce3b86bb00ffd64e9b415d6d1933ef
Federico Macías <fma.abogado@gmail.com>
2022-08-10 10:32:27 -0500

    Protected the home route to users only and redirected to the login and sign in for not users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 //pages and components
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
+  //We access the users in the state
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-      <div className="pages">
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-        </Routes>
-      </div>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
